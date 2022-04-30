1a511ce0e8bb11e4f09119b1f69d30be0637fff8
com_gunzi <nolispnolife@gmail.com>
2022-04-30 23:46:03 +0900

    socket の作成関数を実装

---
+++ server.cpp
@@ -0,0 +1,90 @@
+#include <asm-generic/socket.h>
+#include <cstddef>
+#include <cstdio>
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <ctype.h>
+#include <errno.h>
+#include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+
+// todo: シグナルの捕捉を追加する
+int serverSocket(const char *portnm) {
+  char nbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+  struct addrinfo hints, *res0;
+  int soc, opt, errcode;
+  socklen_t opt_len;
+
+  // アドレス情報のヒントをゼロクリア
+
+  memset(&hints, 0, sizeof(0));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  // アドレス情報の決定
+  errcode = getaddrinfo(NULL, portnm, &hints, &res0);
+  if (errcode != 0) {
+    fprintf(stderr, "getaddrinfo():%s\n", gai_strerror(errcode));
+    return -1;
+  }
+  errcode = getnameinfo(res0->ai_addr, res0->ai_addrlen, nbuf, sizeof(nbuf),
+                        sbuf, sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV);
+  if (errcode != 0) {
+    fprintf(stderr, "getnameinfo():%s\n", gai_strerror(errcode));
+    freeaddrinfo(res0);
+    return -1;
+  }
+  fprintf(stderr, "port=%s\n", sbuf);
+
+  // ソケットの生成
+  soc = socket(res0->ai_family, res0->ai_socktype, res0->ai_protocol);
+  if (soc == -1) {
+    perror("socket");
+    freeaddrinfo(res0);
+    return -1;
+  }
+
+  // ソケットオプション(再利用フラグ)設定
+  opt = 1;
+  opt_len = sizeof(opt);
+  if (setsockopt(soc, SOL_SOCKET, SO_REUSEADDR, &opt, opt_len) == -1) {
+    perror("setsockopt");
+    close(soc);
+    freeaddrinfo(res0);
+    return -1;
+  }
+
+  // ソケットにアドレスを指定
+  if (bind(soc, res0->ai_addr, res0->ai_addrlen) == -1) {
+    perror("bind");
+    close(soc);
+    freeaddrinfo(res0);
+    return -1;
+  }
+
+  // アクセスバックログの指定
+  if (listen(soc, SOMAXCONN) == -1) {
+    perror("listen");
+    close(soc);
+    freeaddrinfo(res0);
+    return -1;
+  }
+  freeaddrinfo(res0);
+  return soc;
+}
+
+void acceptLoop(int soc) {}
+
+int main() {}
