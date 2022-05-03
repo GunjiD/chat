82a08889d029235dcc6e11184e7256531d68da92
com_gunzi <nolispnolife@gmail.com>
2022-05-03 23:27:27 +0900

    client.cpp に sendRecvLoop() を実装

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
+#include <bits/types/struct_timeval.h>
 #include <cstddef>
 #include <cstdio>
 #include <sys/param.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -67,4 +69,78 @@ int clientSocket(const char *hostnm, const char *portnm) {
   freeaddrinfo(res0);
 
   return soc;
+}
+
+void sendRecvLoop(int soc) {
+  struct timeval timeout;
+  char buf[512];
+  int end, width;
+  ssize_t len;
+  fd_set mask, ready;
+
+  // select()用マスク
+  FD_ZERO(&mask);
+  // ソケットディスクリプタをセット
+  FD_SET(soc, &mask);
+  // 標準入力をセット
+  FD_SET(0, &mask);
+  width = soc + 1;
+  // 送受信
+  for (end = 0;;) {
+    // マスクの代入
+    ready = mask;
+    // タイムアウト値のセット
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+    switch (select(width, (fd_set *)&ready, nullptr, nullptr, &timeout)) {
+    case -1:
+      // エラー
+      perror("select");
+      break;
+    case 0:
+      // timeout
+      break;
+    default:
+      // レディ有り
+      // ソケットレディ
+      if (FD_ISSET(soc, &ready)) {
+        // 受信
+        len = recv(soc, buf, sizeof(buf), 0);
+        if (len == -1) {
+          // エラー
+          perror("recv");
+          end = 1;
+          break;
+        } else if (len == 0) {
+          // エンド・オブ・ファイル
+          fprintf(stderr, "recv:EOF\n");
+          end = 1;
+          break;
+        }
+        // 文字列化・表示
+        buf[len] = '\0';
+        printf("> %s", buf);
+      }
+      // 標準入力レディ
+      if (FD_ISSET(0, &ready)) {
+        // 標準入力から1行読み込み
+        fgets(buf, sizeof(buf), stdin);
+        if (feof(stdin)) {
+          end = 1;
+          break;
+        }
+        // 送信
+        len = send(soc, buf, strlen(buf), 0);
+        if (len == -1) {
+          perror("send");
+          end = 1;
+          break;
+        }
+      }
+      break;
+    }
+    if (end) {
+      break;
+    }
+  }
 }
